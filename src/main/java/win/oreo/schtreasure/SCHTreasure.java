5fa3e7c53299f9b1399923b5860e8b222548ee85
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-09-14 19:16:35 +0900

    Initial commit

---
+++ src/main/java/win/oreo/schtreasure/SCHTreasure.java
@@ -0,0 +1,17 @@
+package win.oreo.schtreasure;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SCHTreasure extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
